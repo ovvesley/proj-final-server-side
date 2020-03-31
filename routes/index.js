e7ea5ca6a2be301f08f1bfe5980358bc2bd34af6
Wesley Ferreira <wsf.ley@gmail.com>
2020-03-31 12:21:50 -0300

    Initial commit: express generator

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  
+  res.json({hello: "world"})
+
+});
+
+module.exports = router;
