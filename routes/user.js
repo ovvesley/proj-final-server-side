ee9a08b9e6256030c144f87761b4b5b14fe5aac3
amaral220x <biel.ilha2021@gmail.com>
2020-04-21 00:22:58 -0300

    Correções relativas a inclusão e edição do usuário

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,26 @@
 var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
-var UserSchema = require("../models/User");
+var UserModel = require("../models/User");
 
 /**
  * @swagger
- * /user: 
+ * /user:
  *  post:
  *    tags:
  *    - user
  *    summary: Create a user
  *    description: Register a new user on the database via sing up page
  *    operationId: createUser
- *    parameters: 
+ *    parameters:
  *    - in: body
  *      name: body
  *      description: create user object
  *      required: true
- *      schema: 
+ *      schema:
  *        $ref: '#/definitions/User'
- *      responses: 
- *        '200': 
+ *      responses:
+ *        '200':
  *          description: Success on creating the new user
  *        '400':
  *          description: Error creating new user
@@ -28,13 +28,26 @@ var UserSchema = require("../models/User");
 
 router.post("/", async (req, res) => {
   try {
-    const newUser = await UserSchema.create(req.body);
-    return res.send(newUser);
+    const { 
+      login, 
+      password 
+    } = req.body;
+    if (!login || !password) {
+      res.status(400);
+      return res.send({
+        msg: "Oops, you forgot the login or password!",
+      });
+    } else {
+      var newUser = new UserModel(req.body);
+      newUser.password = newUser.generateHash(password);
+      await UserModel.create(req.body);
+      console.log(newUser);
+      return res.send({ message: "User created successfully", user: newUser });
+    }
   } catch (err) {
-    return res.status(400).send({ error: "Error creating new user" });
+    return res.status(400).send({ error: err.message });
   }
 });
-
 
 /**
  * @swagger
@@ -48,7 +61,7 @@ router.post("/", async (req, res) => {
  *    parameters:
  *      - name: userId
  *        in: path
- *        description: refferrence to the user object which it's info needs to be updated 
+ *        description: refferrence to the user object which it's info needs to be updated
  *        required: true
  *        type: string
  *      - in: body
@@ -60,37 +73,34 @@ router.post("/", async (req, res) => {
  *        responses:
  *          '200':
  *            description: okay
- *          '400': 
+ *          '400':
  *            description: Error updating user
  */
 
 router.put("/:userId", async (req, res) => {
   try {
-    const {
-      login,
-      password,
-      experienceDays,
-      accountPlanType,
-      systems,
-    } = req.body;
-    const id = mongoose.Types.ObjectId(req.params.userId);
-    const newUser = await UserSchema.findByIdAndUpdate(
-      id,
-      {
-        login,
-        password,
-        experienceDays,
-        accountPlanType,
-        systems,
-      },
-      { new: true }
-    );
-    return res.send({ status: "ok" });
+    let currentUser = await UserModel.findOne({_id:req.params.userId});
+    
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0){ 
+      res.status(406);
+      return res.send({message: 'Insira informações para atualizar'});
+    }
+
+    for (const prop in req.body) {
+      if (req.body.hasOwnProperty(prop) && prop !== 'password') {
+        console.log(req.body[prop]);
+        currentUser[prop] = req.body[prop];
+      }
+    }
+    currentUser.save();
+
+    res.status(200);
+    return res.send({ message: `Usuário ${req.body.login} atualizado com sucesso`, user: currentUser});
   } catch (err) {
-    return res.status(400).send({ error: "Error updating user" });
+    console.log(err);
+    return res.status(400).send({ message: "Error updating user" });
   }
 });
-
 
 /**
  * @swagger
@@ -104,20 +114,20 @@ router.put("/:userId", async (req, res) => {
  *    parameters:
  *      - name: userId
  *        in: path
- *        description: refferrence to the user object which will be deleted 
+ *        description: refferrence to the user object which will be deleted
  *        required: true
  *        type: string
  *        responses:
  *          '200':
- *            description: User deleted            
- *          '400': 
+ *            description: User deleted
+ *          '400':
  *            description: Error deleting user
  */
 router.delete("/:userId", async (req, res) => {
   try {
     const id = mongoose.Types.ObjectId(req.params.userId);
     console.log(id);
-    await UserSchema.findByIdAndRemove(id);
+    await UserModel.findByIdAndRemove(id);
     return res.send();
   } catch (err) {
     return res.status(400).send({ error: "Error deleting user" });
