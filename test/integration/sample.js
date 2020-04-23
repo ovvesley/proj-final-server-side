c91c9fc1a08be7dc195a33a922d0eda3575ff1d1
Wesley Ferreira <wsf.ley@gmail.com>
2020-04-23 14:22:38 -0300

    Integration Test: create sample test

---
+++ test/integration/sample.js
@@ -0,0 +1,44 @@
+const proxyquire = require("proxyquire");
+const config = require("./config");
+const toolsdb = require("../../tools/mongooseTools");
+const expect = require("chai").expect;
+
+const stubs = {
+  "./config": config,
+};
+
+const app = proxyquire("../../app", stubs);
+
+const request = require("supertest")(app);
+
+describe("SAMPLE: sample test example", function () {
+  beforeEach(function (done) {
+    toolsdb.clearMongooseDataBase();
+    done();
+  });
+
+  it("GET / - index sample test example", (done) => {
+    request
+      .get("/")
+      .set("Accept", "application/json")
+      .end((err, response) => {
+        if (err) done(err);
+        let { body } = response;
+
+        let expectedRes = { hello: "world" };
+
+        expect(body).to.deep.equal(expectedRes);
+        done();
+      });
+  });
+
+  afterEach(function (done) {
+    toolsdb.clearMongooseDataBase();
+    done();
+  });
+
+  after(async function () {
+    await toolsdb.dropDataBaseMongoose();
+    await toolsdb.disconnectMongoose();
+  });
+});
