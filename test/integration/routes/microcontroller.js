780420e835864b7fdcf4d1d763f3221f414fdef8
amaral220x <biel.ilha2021@gmail.com>
2020-05-11 15:44:24 -0300

    Microcontroller's integration tests: DELETE cases

--- test/integration/routes/microcontroller.js
+++ test/integration/routes/microcontroller.js
@@ -185,7 +185,7 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
     app = proxyquire("../../../app", stubs);
     request = require("supertest")(app);
     done();
-  })
+  });
   beforeEach(function (done) {
     toolsdb
       .clearMongooseDataBase()
@@ -204,8 +204,8 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
       .post("/microcontroller")
       .set("Accept", "application/json")
       .send({
-        nameMicrocontroller:"testeintegracao",
-        type:"led",
+        nameMicrocontroller: "testeintegracao",
+        type: "led",
       })
       .end((err, response) => {
         if (err) {
@@ -213,15 +213,15 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
         }
 
         let { body, status } = response;
-        var id = body.microcontroller._id; 
+        var id = body.microcontroller._id;
         console.log(body.microcontroller._id);
         expect(status).equals(200);
         expect(body).to.deep.include({ status: "Success" });
         request
-          .put("/microcontroller/"+id)
+          .put("/microcontroller/" + id)
           .set("Accept", "application/json")
           .send({
-            nameMicrocontroller:"atualizado"
+            nameMicrocontroller: "atualizado",
           })
           .end((err, response) => {
             if (err) {
@@ -240,8 +240,8 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
       .post("/microcontroller")
       .set("Accept", "application/json")
       .send({
-        nameMicrocontroller:"testeintegracao",
-        type:"led",
+        nameMicrocontroller: "testeintegracao",
+        type: "led",
       })
       .end((err, response) => {
         if (err) {
@@ -249,11 +249,11 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
         }
 
         let { body, status } = response;
-        var id = body.microcontroller._id; 
+        var id = body.microcontroller._id;
         expect(status).equals(200);
         expect(body).to.deep.include({ status: "Success" });
         request
-          .put("/microcontroller/"+id)
+          .put("/microcontroller/" + id)
           .set("Accept", "application/json")
           .end((err, response) => {
             if (err) {
@@ -272,7 +272,7 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
       .post("/microcontroller")
       .set("Accept", "application/json")
       .send({
-        nameMicrocontroller:"testeintegracao",
+        nameMicrocontroller: "testeintegracao",
         type: "testeintegracao",
       })
       .end((err, response) => {
@@ -287,7 +287,7 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
           .post("/microcontroller")
           .set("Accept", "application/json")
           .send({
-            nameMicrocontroller:"testeintegracao2",
+            nameMicrocontroller: "testeintegracao2",
             type: "led",
           })
           .end((err, response) => {
@@ -296,20 +296,20 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
             }
             let { body, status } = response;
             expect(status).equals(200);
-            expect(body).to.deep.include({ status:"Success" });
+            expect(body).to.deep.include({ status: "Success" });
             request
-              .put("/microcontroller/"+id)
+              .put("/microcontroller/" + id)
               .set("Accept", "application/json")
               .send({
-                nameMicrocontroller:"testeintegracao2",
+                nameMicrocontroller: "testeintegracao2",
               })
-              .end((err, response) =>{
-                if(err) {
+              .end((err, response) => {
+                if (err) {
                   done(err);
                 }
                 let { body, status } = response;
                 expect(status).equals(403);
-                expect(body).to.deep.include({ error:"Forbidden"})
+                expect(body).to.deep.include({ error: "Forbidden" });
                 done();
               });
           });
@@ -327,6 +327,97 @@ describe("SUITE: /microcontroller/:microcontrollerId - microcontroller update", 
       .catch((err) => {
         done(err);
       });
+  });
+
+  after(async function () {
+    await toolsdb.dropDataBaseMongoose();
+    await toolsdb.disconnectMongoose();
+  });
+});
+describe("SUITE: /microcontroller - delete microcontroller ", function () {
+  var app;
+  var request;
+
+  before(function (done) {
+    app = proxyquire("../../../app", stubs);
+    request = require("supertest")(app);
+    done();
+  });
+  beforeEach(function (done) {
+    toolsdb
+      .clearMongooseDataBase()
+      .then((response) => {
+        if (response) {
+          done();
+        }
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it("DELETE /microcontroller/:microcontrollerId - delete microcontroller - Success ", (done) => {
+    request
+      .post("/microcontroller")
+      .set("Accept", "application/json")
+      .send({
+        nameMicrocontroller: "testeintegracao",
+        type: "led",
+      })
+      .end((err, response) => {
+        if (err) {
+          done(err);
+        }
+        let { body, status } = response;
+        var id = body.microcontroller._id;
+        expect(status).equals(200);
+        expect(body).to.deep.include({ status: "Success" });
+        request.delete("/microcontroller/" + id).end((err, response) => {
+          if (err) {
+            done(err);
+          }
+          expect(status).equals(200);
+          expect(body).to.deep.include({ status: "Success" });
+          done();
+        });
+      });
+  });
+  it("DELETE /microcontroller/:microcontrollerId - microcontroller isnt deleted - deleting a microcontroller that already deleted ", (done) => {
+    request
+      .post("/microcontroller")
+      .set("Accept", "application/json")
+      .send({
+        nameMicrocontroller: "testeintegracao",
+        type: "led",
+      })
+      .end((err, response) => {
+        if (err) {
+          done(err);
+        }
+        let { body, status } = response;
+        var id = body.microcontroller._id;
+        expect(status).equals(200);
+        expect(body).to.deep.include({ status: "Success" });
+        request.delete("/microcontroller/" + id).end((err, response) => {
+          if (err) {
+            done(err);
+          }
+          expect(status).equals(200);
+          request.delete("/microcontroller/" + id).end((err, response) => {
+            if (err) {
+              done(err);
+            }
+            let { body, status } = response;
+            expect(status).equals(400);
+            done();
+          });
+        });
+      });
+  });
+
+  afterEach(function (done) {
+    toolsdb.clearMongooseDataBase();
+    done();
   });
 
   after(async function () {
