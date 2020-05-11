8bdce7986874c5bb24ac82377977340f2235e337
enzo-z <ezamoradev@gmail.com>
2020-05-11 07:07:54 -0300

    Requisiçao DELETE rota system (Exclusao de sistema)

--- routes/system.js
+++ routes/system.js
@@ -28,10 +28,10 @@ const reqBodyIsEmpty = (body) =>{
 router.post("/", async (req, res) => {
     const op = 'criação';
     try {
-        const {nameSystem:name, category, microcontrollers} = req.body;
-        console.log(name,microcontrollers, category);
+        const {nameSystem, category, microcontrollers} = req.body;
+        console.log(nameSystem,microcontrollers, category);
         console.log(microcontrollers);
-        if(!name){
+        if(!nameSystem){
             let responseObj = {
                 error: "Forbidden",
                 msg: "Certifique de fornecer um nome para o sistema!"
@@ -41,17 +41,17 @@ router.post("/", async (req, res) => {
             }
                 return res.status(403).send(responseObj);
         }
-        else if (await SystemModel.findOne({nameSystem:name})){
+        else if (await SystemModel.findOne({nameSystem:nameSystem})){
             return res.status(403).send({
                 error:"Forbidden",
-                msg: `O nome de sistema '${name}' já está em uso. Escolha outro!`
+                msg: `O nome de sistema '${nameSystem}' já está em uso. Escolha outro!`
             });
         }
         else{
             const systemCreated  = await SystemModel.create(req.body);
             const responseObj = {
                 status: 'Success',
-                msg: `Sistema [${name} criado com sucesso]`,
+                msg: `Sistema [${systemCreated.nameSystem}] criado com sucesso`,
                 system : systemCreated
             };
             res.status(200).send(responseObj);
@@ -59,11 +59,42 @@ router.post("/", async (req, res) => {
     }catch (err) {
         res.status(400).send(errorHandler(err.message, op));
     }
-})
+});
 
 /**Update System */
 
-/**Deleta system */
 
+/**Delete system */
+router.delete("/:systemId", async(req, res) => {
+    const op = 'exclusão';
+    try {
+        const id = mongoose.Types.ObjectId(req.params.systemId);
+        if(!await SystemModel.findById(id)){
+            return res.status(410).send({
+                error:"Sistema inexistente!",
+                msg: "Não existe sistema correspondente para efetuar exclusão!"
+            });
+        }
+        else{
+            let systemRemoved = await SystemModel.findByIdAndDelete(id);
+            return res.status(200).send({
+                status:"Success",
+                msg: `Sistema [${systemRemoved.nameSystem}] deletado com sucesso`
+            });
+        }
+    } catch (err){
+        console.log(err);
+        return res.status(400).send(errorHandler(err.message, op));
+    }
+});
+
+/**Not found */
+router.use("/", async (req, res)=> {
+    res.status(404);
+    res.send( {
+      error: "Not found",
+      msg: "Endpoint não disponível!"
+    });
+  });
 
 module.exports = router;
