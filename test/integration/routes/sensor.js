ad0bad304ed0c3e57d626c104767f98ab2710edb
amaral220x <biel.ilha2021@gmail.com>
2020-05-27 19:04:44 -0300

    Sensor's integrationTest: PUT cases

--- test/integration/routes/sensor.js
+++ test/integration/routes/sensor.js
@@ -145,3 +145,168 @@ describe("SUITE: /sensor - sensor creation ", function () {
       await toolsdb.disconnectMongoose();
     });
   });
+  describe("SUITE: /sensor/:sensorId - sensor update", function () {
+    var app;
+    var request;
+  
+    before(function (done) {
+      app = proxyquire("../../../app", stubs);
+      request = require("supertest")(app);
+      done();
+    });
+    beforeEach(function (done) {
+      toolsdb
+        .clearMongooseDataBase()
+        .then((response) => {
+          if (response) {
+            done();
+          }
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+  
+    it("PUT: /sensor/sensorId - updating a sensor - Success", (done) => {
+      request
+        .post("/sensor")
+        .set("Accept", "application/json")
+        .send({
+            nameSensor: "sensor",
+            digitalValue: 100,
+	        analogValue: 1, 
+	        portNumber:11,
+        })
+        .end((err, response) => {
+          if (err) {
+            done(err);
+          }
+  
+          let { body, status } = response;
+          var id = body.sensor._id;
+          console.log(body.sensor._id);
+          expect(status).equals(200);
+          expect(body).to.deep.include({ status: "Success" });
+          request
+            .put("/sensor/" + id)
+            .set("Accept", "application/json")
+            .send({
+              nameSensor: "atualizado",
+            })
+            .end((err, response) => {
+              if (err) {
+                done(err);
+              }
+              let { body, status } = response;
+              expect(status).equals(200);
+              expect(body).to.deep.include({ status: "Success" });
+              done();
+            });
+        });
+    });
+  
+    it("PUT: /sensor/sensorId - sensor isnt updated - req body is empty", (done) => {
+      request
+        .post("/sensor")
+        .set("Accept", "application/json")
+        .send({
+            nameSensor: "sensor",
+            digitalValue: 100,
+	        analogValue: 1, 
+	        portNumber:11,
+        })
+        .end((err, response) => {
+          if (err) {
+            done(err);
+          }
+  
+          let { body, status } = response;
+          var id = body.sensor._id;
+          expect(status).equals(200);
+          expect(body).to.deep.include({ status: "Success" });
+          request
+            .put("/sensor/" + id)
+            .set("Accept", "application/json")
+            .end((err, response) => {
+              if (err) {
+                done(err);
+              }
+              let { body, status } = response;
+              expect(status).equals(403);
+              expect(body).to.deep.include({ error: "Forbidden" });
+              done();
+            });
+        });
+    });
+  
+    it("PUT: /sensor/:sensorId - sensor isnt updated -  nameSensor already exists ", (done) => {
+      request
+        .post("/sensor")
+        .set("Accept", "application/json")
+        .send({
+            nameSensor: "sensor",
+            digitalValue: 100,
+	        analogValue: 1, 
+	        portNumber:11,
+        })
+        .end((err, response) => {
+          if (err) {
+            done(err);
+          }
+          let { body, status } = response;
+          var id = body.sensor._id;
+          expect(status).equals(200);
+          expect(body).to.deep.include({ status: "Success" });
+          request
+            .post("/sensor")
+            .set("Accept", "application/json")
+            .send({
+                nameSensor: "sensor2",
+                digitalValue: 100,
+                analogValue: 1, 
+                portNumber:11,
+            })
+            .end((err, response) => {
+              if (err) {
+                done(err);
+              }
+              let { body, status } = response;
+              expect(status).equals(200);
+              expect(body).to.deep.include({ status: "Success" });
+              request
+                .put("/sensor/" + id)
+                .set("Accept", "application/json")
+                .send({
+                  nameSensor: "sensor2"
+                })
+                .end((err, response) => {
+                  if (err) {
+                    done(err);
+                  }
+                  let { body, status } = response;
+                  expect(status).equals(403);
+                  expect(body).to.deep.include({ error: "Forbidden" });
+                  done();
+                });
+            });
+        });
+    });
+  
+    afterEach(function (done) {
+      toolsdb
+        .clearMongooseDataBase()
+        .then((res) => {
+          if (res) {
+            done();
+          }
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+  
+    after(async function () {
+      await toolsdb.dropDataBaseMongoose();
+      await toolsdb.disconnectMongoose();
+    });
+  });
